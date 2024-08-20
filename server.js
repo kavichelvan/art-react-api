8e32f04a5182b736cde7827e703ea0fda6438284
Kavichelvan P <41743539+kavichelvan@users.noreply.github.com>
2024-08-20 20:29:30 +0530

    Swagger added (#4)
    * UI-API integrated (#1)
    
    * squash commit after git reset --soft HEAD~4
    
    * removed duplicate in readme
    
    * UI api integrated (#2)
    
    * squash commit after git reset --soft HEAD~4
    
    * removed duplicate in readme
    
    * added swagger documentation
    
    * swagger setup already there

--- server.js
+++ server.js
@@ -1,29 +1,94 @@
 // server.js
 
-const express = require('express');
-const cors = require('cors');
+const express = require("express");
+const cors = require("cors");
+const swaggerJsDoc = require("swagger-jsdoc");
+const swaggerUi = require("swagger-ui-express");
+
 const app = express();
 const port = 5000;
 
 // Enable CORS
 app.use(cors());
 
+// Swagger setup done
+const swaggerOptions = {
+  swaggerDefinition: {
+    openapi: "3.0.0",
+    info: {
+      title: "Sample API",
+      version: "1.0.0",
+      description: "A sample API documentation",
+    },
+    servers: [
+      {
+        url: "http://localhost:5000",
+        description: "Local server",
+      },
+    ],
+  },
+  apis: ["./server.js"], // Files containing annotations for the Swagger doc
+};
+
+const swaggerDocs = swaggerJsDoc(swaggerOptions);
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocs));
+
+/**
+ * @swagger
+ * components:
+ *   schemas:
+ *     DataMap:
+ *       type: object
+ *       properties:
+ *         message:
+ *           type: string
+ *         data:
+ *           type: array
+ *           items:
+ *             type: integer
+ *
+ * /api/dataMap:
+ *   get:
+ *     summary: Get sample data map
+ *     responses:
+ *       200:
+ *         description: A data map object
+ *         content:
+ *           application/json:
+ *             schema:
+ *               $ref: '#/components/schemas/DataMap'
+ *
+ * /api/data:
+ *   get:
+ *     summary: Get simple message
+ *     responses:
+ *       200:
+ *         description: A simple message
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 message:
+ *                   type: string
+ */
+
 // Sample data
 const dataMap = {
-  message: 'Hello from the backend!',
-  data: [1, 2, 3, 4, 5]
+  message: "Hello from the backend!",
+  data: [1, 2, 3, 4, 5],
 };
 
 const data = {
-    message: 'Data from backend!'
-}
+  message: "Data from backend!",
+};
 
 // API route
-app.get('/api/dataMap', (req, res) => {
+app.get("/api/dataMap", (req, res) => {
   res.json(dataMap);
 });
 
-app.get('/api/data', (req, res) => {
+app.get("/api/data", (req, res) => {
   res.json(data);
 });
 
